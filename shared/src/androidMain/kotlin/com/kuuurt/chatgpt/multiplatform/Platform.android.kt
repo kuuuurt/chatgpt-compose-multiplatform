f88ee614e98e65650706c440c0eb2e278f449b9c
Kurt Renzo Acosta <18444033+kuuuurt@users.noreply.github.com>
2023-08-12 15:42:56 +0800

    Removes platform

--- shared/src/androidMain/kotlin/com/kuuurt/chatgpt/multiplatform/Platform.android.kt
+++
@@ -1,9 +0,0 @@
-package com.kuuurt.chatgpt.multiplatform
-
-import com.kuuurt.chatgpt.multiplatform.Platform
-
-class AndroidPlatform : Platform {
-    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
-}
-
-actual fun getPlatform(): Platform = AndroidPlatform()
--- shared/src/desktopMain/kotlin/com/kuuurt/chatgpt/multiplatform/Platform.desktop.kt
+++
@@ -1,9 +0,0 @@
-package com.kuuurt.chatgpt.multiplatform
-
-import com.kuuurt.chatgpt.multiplatform.Platform
-
-class DesktopPlatform : Platform {
-  override val name: String = "Desktop"
-}
-
-actual fun getPlatform(): Platform = DesktopPlatform()
